d415ef4555f9932879a8d31f6210a2a20a220d99
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-04 02:54:32 -0700

    Pass 6/6 tests in 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cTemp = ((temp - 32) * 5/9);
+  if (cTemp % 1 != 0){
+    return +cTemp.toFixed(1);
+  }
+  return cTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fTemp = ((temp * 9/5) + 32);
+  if (fTemp % 1 != 0){
+    return +fTemp.toFixed(1);
+  }
+  return fTemp;
 };
 
 // Do not edit below this line
