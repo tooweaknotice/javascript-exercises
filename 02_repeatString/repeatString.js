11924fe00b24185724ed26ab638457a936765786
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-03 22:56:13 -0700

    Pass 1/6 tests in 02_repeatstring of javascript-exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let outString = ''
+  for (let i = 0; i < num; i++) {
+    outString = outString + string;
+  }
+  return outString;
 };
 
 // Do not edit below this line
