dfc5cbb75d060fdda85241456e39937a3b5a3c48
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-03 23:29:46 -0700

    Pass 1/4 tests in 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reverseString = ``;
+  let arString = string.split(``);
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverseString = reverseString + arString[i];
+  }
+  return reverseString;
 };
 
 // Do not edit below this line
