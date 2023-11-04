ef665015d3acc8eb9226fcf9a07c51860a0cebd6
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-04 01:58:03 -0700

    Pass 6/6 tests in 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(aNum, bNum) {
+  args = [...arguments]
+  const isNegative = (num) => num < 0;
+  const isNonNumber = (num) => typeof num !== 'number';
+  if (args.some(isNegative) || args.some(isNonNumber)) {
+    return `ERROR`;
+  }
+  let sum = 0;
+  let sumRange = Math.abs(aNum - bNum);
+for (let i = 0; i <= sumRange; i++) {
+  if (aNum < bNum) {
+    sum += (aNum + i);
+  }
+  else {
+    sum += (bNum + i);
+  }
+}
+return sum;
 };
 
 // Do not edit below this line
