6114845c226aec266b52280f7021a5a50fa1ae82
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-04 02:12:01 -0700

    Pass 6/6 tests for 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+  if (num % 4 === 0 && (num % 100 !== 0 || num % 400 === 0)) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
