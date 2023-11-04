11d04955bbba0cf29181e93c5aa55475ed8fc48e
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-04 00:32:18 -0700

    Pass 1/7 tests in 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, arg) {
+  let outArray = []
+  for (let i in array) {
+    if (array[i] === arg) {
+      continue;
+    }
+    outArray.push(array[i]);
+  }
+  return outArray;W
 };
 
 // Do not edit below this line
