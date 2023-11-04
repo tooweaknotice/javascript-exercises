09eb281cae7801dd4fe3a3c7db153dd80147fcdf
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-03 23:15:21 -0700

    Pass 5/7 tests in 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,7 @@
 const repeatString = function(string, num) {
+  if (num < 0) {
+    return `ERROR`;
+  }
   let outString = ''
   for (let i = 0; i < num; i++) {
     outString = outString + string;
