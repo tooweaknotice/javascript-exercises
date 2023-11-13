38381020b779ed75e5809cebaf6d9a3ff88336df
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-13 01:39:52 -0800

    Complete 12_findTheOldest in javascript exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const arrPeople = people;
+  arrPeople.sort(ageCompare);
+  console.log(arrPeople[arrPeople.length - 1].name);
+  return arrPeople[arrPeople.length - 1];
 };
+
+function ageCompare(personA, personB){
+  const currentYear = new Date().getFullYear();
+  if (personA.yearOfDeath == undefined){
+    personA.yearOfDeath = currentYear;
+  }
+  if (personB.yearOfDeath == undefined){
+    personB.yearOfDeath = currentYear;
+  }
+  return ((personA.yearOfDeath - personA.yearOfBirth) - (personB.yearOfDeath - personB.yearOfBirth));
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
