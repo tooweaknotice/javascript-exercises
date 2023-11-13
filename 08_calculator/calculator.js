98ff9059f1cc8fe6f2827e9a66537b904c2438da
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-12 23:34:17 -0800

    Complete 08_calculator in javascript-exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, adder) => sum + adder, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, multiplicant) => product * multiplicant, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let out = 1;
+	for (let i = 0; i < b; i++) {
+    out *= a;
+  }
+  return out;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let out = 1;
+  for (let i = 1; i <= a; i++) {
+    out = i * out;
+  }
+  return out;
 };
 
 // Do not edit below this line
