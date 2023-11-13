9dcf697d90789b0a512c6e9e66fa807b6fbd9571
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-13 01:05:33 -0800

    Complete 10_fibonacci in javascript exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+  let num = +a
+  if (num < 0) {
+    return `OOPS`
+  }
+  else if (num === 0){
+    return 0;
+  }
+  else if (num === 1) {
+    return 1;
+  }
+  else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 };
 
 // Do not edit below this line
