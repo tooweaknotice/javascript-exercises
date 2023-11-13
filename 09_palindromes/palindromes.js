31e785caa74e1058ff345605597c256ccd0abd06
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-13 00:50:24 -0800

    Complete 09_palindromes in javascript exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const arrString = [...string];
+  const filteredString = arrString.filter(isAlphanumberic);
+  const cleanString = filteredString.join().toLowerCase();
+  const reverseString = filteredString.reverse().join().toLowerCase();
+  return (cleanString === reverseString);
 };
+
+function isAlphanumberic(char){
+  return ((char >= `A` && char <= `Z`) || 
+  (char >= `a` && char <= `z`) ||
+  (char >= `0` && char <= `9`));
+}
 
 // Do not edit below this line
 module.exports = palindromes;
